4b2500ea7f871e5cad3f92c8a7c53d716242356d
Hugues Bernet-Rollande <hugues@xdev.fr>
2020-10-19 13:31:46 +0200

    Revert "export `ReactTrackingContext`"
    This reverts commit 94e6abbfa0331a8c9c11178e83b05c9acf87e47d.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import track from './trackingHoC';
 
-export { default as withTracking, ReactTrackingContext } from './withTrackingComponentDecorator';
+export { default as withTracking } from './withTrackingComponentDecorator';
 export { default as trackEvent } from './trackEventMethodDecorator';
 export { default as TrackingPropType } from './TrackingPropType';
 export { default as useTracking } from './useTracking';
